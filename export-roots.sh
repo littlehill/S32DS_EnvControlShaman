6e7c6cb3121c0c91d35d650b3e1d262cbd12ed3f
Mirek <mirek.littlehill@damon.com>
2024-06-03 13:43:31 -0700

    update export roots script and update the package list after RTD 4.0.0 install
    Signed-off-by: Mirek <mirek.littlehill@damon.com>

--- export-roots.sh
+++ export-roots.sh
@@ -1,1 +1,16 @@
-../S32DS.3.5/eclipse/eclipsec.exe -application org.eclipse.equinox.p2.director -listInstalledRoots 2>/dev/null >config_roots_K3xx.txt 
+#!/bin/bash
+MAINOUTPUTFILE="config_roots_K3xx.txt"
+
+../S32DS.3.5/eclipse/eclipsec.exe -application org.eclipse.equinox.p2.director -listInstalledRoots 2>/dev/null >$MAINOUTPUTFILE
+RC=$?
+if [ $RC -eq "0" ]; then
+  echo "Export DONE. File: $MAINOUTPUTFILE"
+else
+  echo "Encountered an error. RC: $RC"
+  exit 1;
+fi
+
+#remove the line which reports time at the end
+sed '/Operation completed in/d' -i config_roots_K3xx.txt
+
+exit 0
