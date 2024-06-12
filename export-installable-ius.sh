253d114e57005b53a051a5b2ffb28867e0ecafc7
Mirek <mirek.littlehill@damon.com>
2024-06-12 16:03:12 -0700

    fixup path and counting when listing installable IUs

--- export-installable-ius.sh
+++ export-installable-ius.sh
@@ -2,18 +2,27 @@
 
 echo "- START listting of all IUs available for install. -"
 IU_LISTFILE=""
+REPO_REFLISTFILE="./workspace/repo-file-list.index"
+
+echo "clear index"
+INDEXCOUNT=0
+rm $REPO_REFLISTFILE
+touch $REPO_REFLISTFILE || exit 1;
+
 
 for repo in $(ls /c/NXP/__S32DS_Install-PKGSRC/NXP_repos_extracted | sed '/.zip/d'); do
 
   IU_LISTFILE="./workspace/${repo}-IU-list.txt"
   echo "processing repo: $repo"
+  REPO_FULLPATH="file:/C:/NXP/__S32DS_Install-PKGSRC/NXP_repos_extracted/$repo"
   echo "-- listing available IUs --"
   ../S32DS.3.5/eclipse/eclipsec.exe \
     -application org.eclipse.equinox.p2.director \
-    -repository file:/C:/NXP/__S32DS_Install-PKGSRC/NXP_repos_extracted/$repo \
+    -repository $REPO_FULLPATH \
     -list -noSplash 2>/dev/null > ${IU_LISTFILE}
 
-  
+  echo "${REPO_FULLPATH} ${IU_LISTFILE} $INDEXCOUNT" >> $REPO_REFLISTFILE
+  ((INDEXCOUNT+=1))
 
 #remove the line which reports time at the end
   sed '/Operation completed in/d' -i ${IU_LISTFILE}
