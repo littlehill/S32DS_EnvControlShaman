19a633fb443830630717148b619066b9ea36f18d
Mirek <mirek.littlehill@damon.com>
2024-06-13 14:41:42 -0700

    add target file parameter to export roots and ignore the default output file

--- export-roots.sh
+++ export-roots.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-MAINOUTPUTFILE="config_roots_K3xx.txt"
+
+# Check if the filename is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "target file not specified, using: 'list_of_roots_s32ds.tmp.txt'" 
+  MAINOUTPUTFILE="list_of_roots_s32ds.tmp.txt"
+else
+  MAINOUTPUTFILE=$(realpath "$1")
+  echo "target file for roots export: $MAINOUTPUTFILE"
+fi
 
 ../S32DS.3.5/eclipse/eclipsec.exe -application org.eclipse.equinox.p2.director -listInstalledRoots -noSplash 2>/dev/null >$MAINOUTPUTFILE
 RC=$?
 if [ $RC -eq "0" ]; then
+#remove the line which reports time at the end
+  sed '/Operation completed in/d' -i $MAINOUTPUTFILE
   echo "Export DONE. File: $MAINOUTPUTFILE"
 else
   echo "Encountered an error. RC: $RC"
   exit 1;
 fi
-
-#remove the line which reports time at the end
-sed '/Operation completed in/d' -i config_roots_K3xx.txt
-
 exit 0
