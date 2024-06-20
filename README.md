c7770147a6cf3b38fd0c1659f4baf597dc9f6271
Mirek <mirek.littlehill@damon.com>
2024-06-20 01:23:57 -0700

    first attempt at a guide for install, baby steps

--- README.md
+++ README.md
@@ -16,3 +16,24 @@ what is the plan ? [see the requirements-and-planning.md](requirements-and-plann
 #### Main Goal for alpha
 
 A clean install on a new machine. Kick off the Shaman to use the p2 director of Eclipse to install whatever you need.
+
+## Install steps
+Info: This only works with Win10/11 at the moment.
+
+1. If you do not have it, install Git Bash. You can find it here: [Git Bash Download](https://www.git-scm.com/download/win).
+
+2. Checkout this repository into `/c/NXP/`. If `/c/NXP` does not exist, create it.
+
+3. Install the S32DS from `S32DS.3.5_b220726_win32.x86_64.exe`.
+
+4. Open Git Bash and `cd` into the `/c/NXP/S32DS_EnvControlShaman` folder.
+
+5. Run `./subtasks/bash_env_sanity_check.sh -v`.
+    - Fix any errors before continuing.
+
+6. Run one of the following commands:
+    - `./subtasks/create_list_of_available_repos.sh -outfile generated_list_of_repos.tmp.txt -url <path to the repositories in S3 bucket or on NAS>`
+    - `./subtasks/create_list_of_available_repos.sh -outfile generated_list_of_repos.tmp.txt -dir <the root folder of your local extracted repos>`
+
+7. Run `./automated-s32ds-update_main.sh generated_list_of_repos.tmp.txt` and hope for the best.
+
--- automated-s32ds-update_main.sh
+++ automated-s32ds-update_main.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-echo "collection of steps to install 'semi-manually' "
+#./subtasks/bash_env_sanity_check.sh -v
+echo "- STEP 1 --------------------------------------------------------------------"
+./export-roots.sh local_list_of_roots_s32ds.tmp.txt
 
+echo "- STEP 2 --------------------------------------------------------------------"
+./subtasks/create_workspace_lists_of_installable_units.sh -r ./generated_list_of_repos.tmp.txt -o ./workspace
+
+echo "- STEP 3 --------------------------------------------------------------------"
+./run_shaman_installer_of_roots.sh ./local_list_of_roots_s32ds.tmp.txt ./config_roots_K3xx.txt -i
+
+# sorry that it has to run through the dependencies again, the order of installation is not yet figured out fully
+# but it doe snot take that much time more and fixes it for this instance
+echo "- STEP 4 --------------------------------------------------------------------"
+./export-roots.sh local_list_of_roots_s32ds.tmp.txt
+./run_shaman_installer_of_roots.sh ./local_list_of_roots_s32ds.tmp.txt ./config_roots_K3xx.txt -i
+
+echo "- STEP 5 --------------------------------------------------------------------"
+./export-roots.sh local_list_of_roots_s32ds.tmp.txt
+./run_shaman_installer_of_roots.sh ./local_list_of_roots_s32ds.tmp.txt ./config_roots_K3xx.txt -i -v
+
+echo "- S32DS Shaman DONE ---------------------------------------------------------"
 exit 0
